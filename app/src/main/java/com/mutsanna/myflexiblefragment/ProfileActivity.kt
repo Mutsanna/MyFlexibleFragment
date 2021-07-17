4e3ac8a76bd64f6368e858c2bb37a54a30657a05
Mutsanna <am.qoid@gmail.com>
2021-07-18 00:59:56 +0700

    Initial Commit

---
+++ app/src/main/java/com/mutsanna/myflexiblefragment/ProfileActivity.kt
@@ -0,0 +1,26 @@
+package com.mutsanna.myflexiblefragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ProfileActivity.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ProfileActivity : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile_activity, container, false)
+    }
+
+}
